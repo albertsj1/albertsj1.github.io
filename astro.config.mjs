fed43145ed3eda14f004d3a4453254f7548b00c9
John Alberts <john.m.alberts@gmail.com>
2025-06-05 17:07:21 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alberts.me',
+
   //site: 'https://albertsj1.github.io',
   //base: '/alberts.me',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
